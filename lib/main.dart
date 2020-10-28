50382a52d40c48c8d2b0ab0187e1ef1866e2cec7
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-10-28 13:19:02 -0300

    chore: temperature indicator & assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(App());
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
 }
 
-class MyApp extends StatelessWidget {
+class _AppState extends State<App> {
+  final int _temperature = 110;
+  final String _location = 'Rodeio';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Open Weather',
       debugShowCheckedModeBanner: false,
       home: Container(
-        decoration: BoxDecoration(image: DecorationImage()),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/clear.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Center(
+                    child: Text(
+                      '$_temperature °C',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 60,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      _location,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 60,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
