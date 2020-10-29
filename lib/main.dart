3278a796e1632c53e6e43ec3c530c0d7f037873e
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-10-28 22:14:50 -0300

    chore: text field & api integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:open_weather/config/api.dart';
 
 void main() => runApp(App());
 
@@ -8,8 +13,37 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final int _temperature = 110;
-  final String _location = 'Rodeio';
+  String _location = 'Rodeio';
+  String _weather = 'clear';
+  int _temperature = 110;
+  int _woeid = 123123;
+
+  Future<void> fetchSearch(String input) async {
+    final response = await http.get(searchApiUrl + input);
+    final jsonResponse = json.decode(response.body)[0];
+
+    setState(() {
+      _location = jsonResponse['title'] as String;
+      _woeid = jsonResponse['woeid'] as int;
+    });
+  }
+
+  Future<void> fetchLocation() async {
+    final response = await http.get(locationApiUrl + _woeid.toString());
+    final jsonResponse = json.decode(response.body);
+    final data = jsonResponse['consolidated_weather'][0];
+
+    final formattedWeather = data['weather_state_name']
+        .toString()
+        .replaceAll(' ', '')
+        .trim()
+        .toLowerCase();
+
+    setState(() {
+      _temperature = data['the_temp'].round() as int;
+      _weather = formattedWeather;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +51,9 @@ class _AppState extends State<App> {
       title: 'Open Weather',
       debugShowCheckedModeBanner: false,
       home: Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('lib/assets/clear.png'),
+            image: AssetImage('lib/assets/$_weather.png'),
             fit: BoxFit.cover,
           ),
         ),
@@ -49,7 +83,50 @@ class _AppState extends State<App> {
                     ),
                   ),
                 ],
-              )
+              ),
+              Column(
+                children: [
+                  const SizedBox(
+                    width: 300,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        cursorColor: Colors.white,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: 'Search another location',
+                          alignLabelWithHint: true,
+                          filled: false,
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 2,
+                            ),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 2,
+                            ),
+                          ),
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
